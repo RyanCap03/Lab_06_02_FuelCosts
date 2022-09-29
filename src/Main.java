beba7e859757561696f723bbd27cf66aef139f9c
RyanCap03 <capre@mail.uc.edu>
2022-09-29 13:19:14 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        double gallons = 0;
+        double mpg = 0;
+        double pricePG = 0;
+        double hundred;
+
+        System.out.print("How many gallons of gas are in your vehicle?: ");
+        if (in.hasNextDouble())
+        {
+            gallons = in.nextDouble();
+            in.nextLine();
+        }
+        else
+        {
+            System.out.println("That is not a correct input!");
+        }
+        //-------------------------------------------------------------------
+
+        System.out.print("How many miles per gallon does your car get?: ");
+        if (in.hasNextDouble())
+        {
+            mpg = in.nextDouble();
+            in.nextLine();
+        }
+        else
+        {
+            System.out.println("That is not a correct input!");
+        }
+        //--------------------------------------------------------------------
+
+        System.out.print("How much do you pay for 1 gallon of gas?: ");
+        if (in.hasNextDouble())
+        {
+            pricePG = in.nextDouble();
+            in.nextLine();
+        }
+        else
+        {
+            System.out.println("That is not a correct input!");
+        }
+        //---------------------------------------------------------------------
+
+        hundred = (100/mpg)*pricePG;
+        System.out.println("The cost per 100 miles is: $" + hundred);
+
+
+    }
+}
